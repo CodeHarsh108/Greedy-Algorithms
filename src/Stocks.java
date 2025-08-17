fc30b63f4160bf2a0c9b172634039cbd08deadca
Harsh Patil <itsharshhh6@gmail.com>
2025-08-17 15:06:16 +0530

    Stcoks Question

--- src/Stocks.java
+++ src/Stocks.java
@@ -1,3 +1,28 @@
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
 public class Stocks {
-    
+    public  static int buyMaximumProducts(int[] price, int k){
+        int n = price.length;
+        List<int[]> days = new ArrayList<>();
+        for (int i = 0; i < n; i++){
+            days.add(new int[]{price[i], i+1});
+        }
+        days.sort(Comparator.comparingInt(a -> a[0]));
+        int totalStocks = 0;
+        for (int[] d : days){
+            int stockPrice = d[0];
+            int limit = d[1];
+
+
+            int affordable = k/stockPrice;
+            int buy = Math.min(limit, affordable);
+            totalStocks += buy;
+            k -= buy * stockPrice;
+
+            if (k == 0) break;
+        }
+        return totalStocks;
+    }
 }
